45dea811d52a049ef8af040a0f59ec6f3c79a9d5
lawsuit3310 <aimeet5480@gmail.com>
2022-07-26 20:58:51 +0900

    ASP.NET 프로젝트 생성 및 예제 풀이 ~ p.60

---
+++ Greylag/FrmHelloWorld.aspx.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace Greylag
+{
+    public partial class FrmHelloWorld : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            //ASP.NET 프로그램의 진입점으로 C의 Main 메소드와 같음
+
+        }
+
+        protected void btnClick_Click(object sender, EventArgs e)
+        {
+            txtDisplay.Text = "안녕하세요";
+            Response.Write("안녕하세요 <br>");
+        }
+    }
+}
---
+++ Greylag/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Greylag.Global" Language="C#" %>
