0546c07dc7063187e63747e58fa12476a3cc2bcd
lawsuit3310 <aimeet5480@gmail.com>
2022-07-26 19:58:35 +0900

    레이아웃에 관하여 ~p.104

--- BikeShop/Menu.xaml.cs
+++ BikeShop/Menu.xaml.cs
@@ -31,5 +31,12 @@ namespace BikeShop
                     new Uri("/Contact.xaml",UriKind.Relative)
                 );
         }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            NavigationService.Navigate(
+                new Uri("/Discussion.xaml",UriKind.Relative) //상대 경로의 파일을 찾아감
+                );
+        }
     }
 }
